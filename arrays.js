70dfedc0ba2fa87fa350ebdd87cdf8c661f1d13f
Edsel Caprice <caprice@mail.usf.edu>
2018-05-17 03:32:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -27,7 +27,7 @@ function destructivelyRemoveElementFromBeginningOfArray (array) {
  return array;
 }
  function removeElementFromBeginningOfArray (array) {
-   return array.slice(1);
+   return array.slice(-2);
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
